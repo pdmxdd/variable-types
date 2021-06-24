183d1f8159caf8be80237ae0a61435b670ed0ccf
pdmxdd <pdmxdd@gmail.com>
2021-06-23 23:30:57 -0500

    data type examples

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myInt = 24;
+        System.out.println(myInt);
+
+        double myDouble = 3.14;
+        System.out.println(myDouble);
+
+        boolean myBoolean = true;
+        System.out.println(myBoolean);
+
+        char myChar = 'A';
+        System.out.println(myChar);
+
+        String myString = "Hello there!";
+        System.out.println(myString);
+    }
+}
